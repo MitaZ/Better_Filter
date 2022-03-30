4545ac2ee30fb2df54cbc079d91abcf8b333689e
MizuhashiZ <MitaZ@users.noreply.github.com>
2022-03-30 17:46:02 +0800

    Update GamerSky_DeviceID.js
    测试成功，去掉log打印，准备推送到主分支

--- Quantumult_X/Script/GamerSky_DeviceID.js
+++ Quantumult_X/Script/GamerSky_DeviceID.js
@@ -1,6 +1,7 @@
 function uuidv4() {
   return ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, c =>
-    (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16).toUpperCase());}
+    (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16).toUpperCase());
+}
 
 var body = $request.body;
 var obj = JSON.parse(body);
@@ -11,8 +12,5 @@ obj['deviceType'] = 'iPhone14,5';
 modifiedBody = JSON.stringify(obj);
 
 var modifiedScheme = 'https';
-
-console.log(modifiedScheme);
-console.log(modifiedBody);
 
 $done({body: modifiedBody, scheme: modifiedScheme});

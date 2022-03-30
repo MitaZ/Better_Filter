c4bc6371d5401632607eac852c3f5db45c96c195
MizuhashiZ <MitaZ@users.noreply.github.com>
2022-03-30 17:20:59 +0800

    Update GamerSky_DeviceID.js

--- Quantumult_X/Script/GamerSky_DeviceID.js
+++ Quantumult_X/Script/GamerSky_DeviceID.js
@@ -2,7 +2,7 @@ function uuidv4() {
   return ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, c =>
     (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16).toUpperCase());}
 
-var body = $response.body;
+var body = $request.body;
 var obj = JSON.parse(body);
 
 obj['deviceId'] = uuidv4();

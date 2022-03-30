c15ae06df0bd7e264754efb2fc299728e99c89ae
MizuhashiZ <MitaZ@users.noreply.github.com>
2022-03-30 17:16:53 +0800

    Update GamerSky_DeviceID.js
    由request-header改为request-body

--- Quantumult_X/Script/GamerSky_DeviceID.js
+++ Quantumult_X/Script/GamerSky_DeviceID.js
@@ -2,10 +2,16 @@ function uuidv4() {
   return ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, c =>
     (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16).toUpperCase());}
 
-var modifiedHeaders = $request.headers;
-modifiedHeaders['deviceId'] = uuidv4();
-modifiedHeaders['deviceType'] = 'iPhone14,5';
+var body = $response.body;
+var obj = JSON.parse(body);
+
+obj['deviceId'] = uuidv4();
+obj['deviceType'] = 'iPhone14,5';
+
+modifiedBody = JSON.stringify(obj);
+
+console.log(modifiedBody);
 
 var modifiedScheme = 'https';
 
-$done({scheme: modifiedScheme, headers: modifiedHeaders});
+$done({body: modifiedBody, scheme: modifiedScheme});
